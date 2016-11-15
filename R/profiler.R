b44f274b649f9597b0cbd952c8937f00292a8227
Kirill Müller <krlmlr+r@mailbox.org>
2016-11-15 17:07:56 +0100

    show_profiler_pdf()

--- R/profiler.R
+++ R/profiler.R
@@ -7,3 +7,13 @@ start_profiler <- function(path) {
 stop_profiler <- function() {
   stop_profiler_impl()
 }
+
+#' @export
+show_profiler_pdf <- function(path, focus = NULL) {
+  pprof_exit_code <- system2(
+    "google-pprof",
+    c("--lines", "--evince", shQuote(file.path(R.home("bin"), "exec/R")),
+      if (!is.null(focus)) paste0("--focus=", focus),
+      shQuote(path)),
+    wait = FALSE)
+}
