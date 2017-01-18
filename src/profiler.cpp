63d07d8277643c28cc52e85b169cb6a1e77d04c0
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-18 15:29:12 +0100

    move implementation to source

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -2,6 +2,35 @@
 
 #include "gprofiler_types.h"
 
+
+ProfilerDaisyChain::ProfilerDaisyChain() {
+  options.filter_in_thread = &filter_in_thread;
+  options.filter_in_thread_arg = reinterpret_cast<void*>(this);
+  sigaction(SIGPROF, NULL, &oldact);
+  if (oldact.sa_flags & SA_SIGINFO) {
+    Rcpp::stop("oops");
+  }
+}
+
+const ProfilerOptions& ProfilerDaisyChain::get_options() const {
+  return options;
+}
+
+int ProfilerDaisyChain::filter_in_thread(void* this_) {
+  ProfilerDaisyChain* this_ptr = reinterpret_cast<ProfilerDaisyChain*>(this_);
+  return this_ptr->filter_in_thread();
+}
+
+int ProfilerDaisyChain::filter_in_thread() {
+  struct sigaction myact;
+  sigaction(SIGPROF, NULL, &myact);
+  if (oldact.sa_handler != SIG_DFL && oldact.sa_handler != SIG_IGN) {
+    oldact.sa_handler(SIGPROF);
+  }
+  sigaction(SIGPROF, &myact, NULL);
+  return 1;
+}
+
 using namespace Rcpp;
 
 // [[Rcpp::export]]
