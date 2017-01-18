e11351e737510a191a051af248ef57444bf7c931
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-18 22:59:56 +0100

    get_profiler_traces() merges gprofiler and Rprof traces

--- R/profiler.R
+++ R/profiler.R
@@ -14,6 +14,26 @@ stop_profiler <- function() {
 }
 
 #' @export
+get_profiler_traces <- function(path = "1.prof") {
+  traces <- system2(
+    get_pprof_path(),
+    c("-lines", "-traces", shQuote(path)),
+    stdout = TRUE)
+
+  pprof_nested <-
+    strsplit(paste(traces, collapse = "\n"), "\n-+[+]-+\n")[[1L]][-1L] %>%
+    as.list %>%
+    tibble::enframe(name = "time", value = "gprofiler")
+
+  rprof_nested <- profvis:::parse_rprof(paste0(path, ".out"))$prof %>%
+    tibble::as_tibble %>%
+    tidyr::nest(-time, .key = rprof)
+
+  stopifnot(pprof_nested$time == rprof_nested$time)
+  tibble::as_tibble(cbind(pprof_nested, rprof_nested[-1]))
+}
+
+#' @export
 show_profiler_pdf <- function(path = "1.prof", focus = NULL) {
   pprof_exit_code <- system2(
     get_pprof_path(),
