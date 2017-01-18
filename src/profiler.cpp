c44eb045c1f187125a27b646444d7160bafd0be0
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-18 16:01:25 +0100

    split

--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1,7 +1,6 @@
 #include <Rcpp.h>
 
-#include "gprofiler_types.h"
-
+#include "profiler.h"
 
 ProfilerDaisyChain::ProfilerDaisyChain() {
   options.filter_in_thread = &filter_in_thread;
@@ -37,22 +36,4 @@ void ProfilerDaisyChain::start(const std::string& path) {
 
 void ProfilerDaisyChain::stop() {
   ProfilerStop();
-}
-
-using namespace Rcpp;
-
-// [[Rcpp::export]]
-List start_profiler_impl(CharacterVector path) {
-  if (path.length() != 1)
-    stop("start_profiler() expects scalar path");
-  ProfilerDaisyChain* dc = new ProfilerDaisyChain();
-  dc->start(std::string(path[0]));
-  return List::create(XPtr<ProfilerDaisyChain>(dc));
-}
-
-// [[Rcpp::export]]
-void stop_profiler_impl(List ldc) {
-  XPtr<ProfilerDaisyChain> pdc = ldc[0];
-  ProfilerDaisyChain* dc = pdc.get();
-  dc->stop();
 }
