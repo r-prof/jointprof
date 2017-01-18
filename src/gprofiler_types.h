97c5b278dfa6ce6f96865db1cd5e572081b0de21
Kirill Müller <krlmlr+r@mailbox.org>
2017-01-18 15:08:29 +0100

    move to header

---
+++ src/gprofiler_types.h
@@ -0,0 +1,8 @@
+#ifndef __GPROFILER_GPROFILER_TYPES_H
+#define __GPROFILER_GPROFILER_TYPES_H
+
+#include <Rcpp.h>
+
+#include "profiler.h"
+
+#endif // #ifndef __GPROFILER_GPROFILER_TYPES_H
--- src/profiler.cpp
+++ src/profiler.cpp
@@ -1,47 +1,8 @@
 #include <Rcpp.h>
 
-#include <gperftools/profiler.h>
-
-#include <signal.h>
+#include "gprofiler_types.h"
 
 using namespace Rcpp;
-
-class ProfilerDaisyChain {
-public:
-  ProfilerDaisyChain() {
-    options.filter_in_thread = &filter_in_thread;
-    options.filter_in_thread_arg = reinterpret_cast<void*>(this);
-    sigaction(SIGPROF, NULL, &oldact);
-    if (oldact.sa_flags & SA_SIGINFO) {
-      stop("oops");
-    }
-  }
-
-public:
-  const ProfilerOptions& get_options() const {
-    return options;
-  }
-
-  static int filter_in_thread(void* this_) {
-    ProfilerDaisyChain* this_ptr = reinterpret_cast<ProfilerDaisyChain*>(this_);
-    return this_ptr->filter_in_thread();
-  }
-
-private:
-  int filter_in_thread() {
-    struct sigaction myact;
-    sigaction(SIGPROF, NULL, &myact);
-    if (oldact.sa_handler != SIG_DFL && oldact.sa_handler != SIG_IGN) {
-      oldact.sa_handler(SIGPROF);
-    }
-    sigaction(SIGPROF, &myact, NULL);
-    return 1;
-  }
-
-private:
-  ProfilerOptions options;
-  struct sigaction oldact;
-};
 
 // [[Rcpp::export]]
 void start_profiler_impl(CharacterVector path) {
