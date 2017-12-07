a1c126addb5c66a1d36219b0cee011dae908218c
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-07 08:47:54 +0100

    add simple integration test

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(gprofiler)
+
+test_check("gprofiler")
---
+++ tests/testthat/test-integration.R
@@ -0,0 +1,16 @@
+context("integration")
+
+test_that("simple integration test", {
+  path <- tempfile("gprofiler", fileext = ".prof")
+
+  start_profiler(path)
+
+  con <- DBI::dbConnect(RSQLite::SQLite(), ":memory:")
+  invisible(lapply(1:100, function(x)
+    DBI::dbWriteTable(con, paste0("iris", x), iris)))
+  DBI::dbDisconnect(con)
+
+  stop_profiler()
+
+  get_profiler_traces(path)
+})
