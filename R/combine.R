f25e04134b0922465f27900580430b7745d9455f
Kirill Müller <krlmlr@mailbox.org>
2017-12-25 14:21:37 +0100

    snapshot, somehow traces cannot be matched

--- R/combine.R
+++ R/combine.R
@@ -1,4 +1,119 @@
 combine_profiles <- function(path, prof_path, out_path) {
+  ds_rprof <- profile::read_rprof(out_path)
+
+  proto_path <- tempfile("gprofiler", fileext = ".pb.gz")
+  system2(
+    get_pprof_path(),
+    c("-proto", "-output", shQuote(proto_path), shQuote(prof_path))
+  )
+  ds_pprof <- profile::read_pprof(proto_path)
+
+  stopifnot(sum(ds_pprof$samples$value) == sum(ds_rprof$samples$value))
+  stopifnot(ds_rprof$samples$value == 1)
+
+  ds_pprof <- shift_ids(ds_pprof, ds_rprof)
+  ds_rprof <- expand_samples(ds_rprof)
+  ds_pprof <- expand_samples(ds_pprof)
+
+  ds_combined <- combine_ds(ds_rprof, ds_pprof)
+  ds_merged <- patch_combined_ds(ds_combined)
+  ds_pruned <- prune_ds(ds_merged)
+
+  profile::write_rprof(ds_pruned, path)
+}
+
+shift_ids <- function(ds, ds_base) {
+  d_location_id <- max(ds_base$locations$location_id)
+  d_function_id <- max(ds_base$functions$function_id)
+
+  ds$samples$locations <- map(ds$samples$locations, function(x) {
+    x$location_id <- x$location_id + d_location_id
+    x
+  })
+  ds$locations$location_id <- ds$locations$location_id + d_location_id
+
+  ds$locations$function_id <- ds$locations$function_id + d_function_id
+  ds$functions$function_id <- ds$functions$function_id + d_function_id
+
+  profile::validate_profile_v1(ds)
+  ds
+}
+
+expand_samples <- function(ds) {
+  new_samples <- tibble::tibble(
+    value = 1L,
+    locations = rep(ds$samples$locations, ds$samples$value)
+  )
+
+  ds$samples <- new_samples
+  profile::validate_profile_v1(ds)
+  ds
+}
+
+combine_ds <- function(ds_rprof, ds_pprof) {
+  ds_rprof$samples$.pprof_locations <- ds_pprof$samples$locations
+  ds_rprof$locations <- tibble::as_tibble(rbind(
+    strip_dots(ds_rprof$locations), strip_dots(ds_pprof$locations)
+  ))
+  ds_rprof$functions <- tibble::as_tibble(rbind(
+    strip_dots(ds_rprof$functions), strip_dots(ds_pprof$functions)
+  ))
+  profile::validate_profile_v1(ds_rprof)
+  ds_rprof
+}
+
+strip_dots <- function(x) {
+  x[grep("^[.]", names(x))] <- NULL
+  x
+}
+
+patch_combined_ds <- function(ds_combined) {
+  ds_combined$samples$.rprof_locations <- ds_combined$samples$locations
+
+  . <- merge(ds_combined$locations, ds_combined$functions, by = "function_id", all.x = TRUE)
+  . <- tibble::as_tibble(.)
+  locations_flat <- .
+
+  ds_combined$locations <- map2(
+    ds_combined$samples$.rprof_locations,
+    ds_combined$samples$.pprof_locations,
+    patch_locations,
+    list(locations_flat)
+  )
+  ds_combined
+}
+
+patch_locations <- function(rprof_locations, pprof_locations, locations_flat) {
+  . <- rprof_locations
+  . <- tibble::rowid_to_column(., "rprof_id")
+  . <- merge(., locations_flat, by = "location_id", sort = FALSE)
+  . <- .[order(.$rprof_id), ]
+  . <- tibble::as_tibble(., rownames = NULL)
+  rprof_locations_full <- .
+  stopifnot(rprof_locations$location_id == rprof_locations_full$location_id)
+
+  if (all(rprof_locations_full$name != ".Call")) {
+    return(rprof_locations)
+  }
+
+  . <- pprof_locations
+  . <- tibble::rowid_to_column(., "pprof_id")
+  . <- merge(., locations_flat, by = "location_id", sort = FALSE)
+  . <- .[order(.$pprof_id), ]
+  . <- tibble::as_tibble(., rownames = NULL)
+  pprof_locations_full <- .
+  stopifnot(pprof_locations$location_id == pprof_locations_full$location_id)
+
+
+  browser()
+  # - construct new location_id sequence for every table
+}
+
+prune_ds <- function(ds) {
+  # - prune unused locations and functions
+}
+
+function() {
   rprof <- read_rprof(out_path)
   pprof_dfs <- read_pprof(prof_path, get_n_traces(rprof))
 
--- R/gprofiler-package.R
+++ R/gprofiler-package.R
@@ -1,3 +1,4 @@
 #' @useDynLib gprofiler,.registration = TRUE
 #' @importFrom Rcpp sourceCpp
+#' @import rlang
 "_PACKAGE"
