d3daf1eb2016a8c2ea1f642b0075229f998c1167
Kirill Müller <krlmlr+r@mailbox.org>
2017-12-27 02:52:08 +0100

    add missing .sty file

--- tic.R
+++ tic.R
@@ -1,10 +1,4 @@
-add_package_checks(
-  warnings_are_errors = FALSE,
-  args = c(
-    "--no-manual", "--as_cran",
-    if (Sys.getenv("TRAVIS_OS_NAME") == "osx") "--no-build-vignettes"
-  )
-)
+add_package_checks(warnings_are_errors = FALSE)
 
 if (ci()$get_branch() == "master" && Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
   # pkgdown documentation can be built optionally. Other example criteria:
