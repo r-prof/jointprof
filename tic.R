200b4d757e4eb789aea06702dcd6453310fba132
Kirill Müller <krlmlr+r@mailbox.org>
2018-01-03 10:34:51 +0100

    always build pkgdown

--- tic.R
+++ tic.R
@@ -1,15 +1,19 @@
-add_package_checks(warnings_are_errors = FALSE)
+add_package_checks()
 
-if (ci()$get_branch() == "master" && Sys.getenv("BUILD_PKGDOWN") != "" && Sys.getenv("id_rsa") != "") {
-  # pkgdown documentation can be built optionally. Other example criteria:
-  # - `inherits(ci(), "TravisCI")`: Only for Travis CI
-  # - `ci()$is_tag()`: Only for tags, not for branches
-  # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
-  # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
-  get_stage("before_deploy") %>%
-    add_step(step_setup_ssh())
-
+if (Sys.getenv("BUILD_PKGDOWN") != "") {
   get_stage("deploy") %>%
-    add_step(step_build_pkgdown()) %>%
-    add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+    add_step(step_build_pkgdown())
+
+  if (ci()$get_branch() == "master" && Sys.getenv("id_rsa") != "") {
+    # pkgdown documentation can be built optionally. Other example criteria:
+    # - `inherits(ci(), "TravisCI")`: Only for Travis CI
+    # - `ci()$is_tag()`: Only for tags, not for branches
+    # - `Sys.getenv("BUILD_PKGDOWN") != ""`: If the env var "BUILD_PKGDOWN" is set
+    # - `Sys.getenv("TRAVIS_EVENT_TYPE") == "cron"`: Only for Travis cron jobs
+    get_stage("before_deploy") %>%
+      add_step(step_setup_ssh())
+
+    get_stage("deploy") %>%
+      add_step(step_push_deploy(path = "docs", branch = "gh-pages"))
+  }
 }
