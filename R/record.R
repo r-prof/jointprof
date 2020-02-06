de78970f7436f8a94fbc9632a18901ca15e63893
Kirill Müller <krlmlr+r@mailbox.org>
2020-02-06 03:13:47 +0100

    - New `comingle_pprof()` and `comingle_rprof()`, for use with proffer.

---
+++ R/record.R
@@ -0,0 +1,38 @@
+#' @title Profile R and native code and record pprof samples.
+#' @export
+#' @description Run R code and record pprof samples
+#'   from both R's and the native tracebacks.
+#'   This function uses [start_profiler()] and [stop_profiler()]
+#'   to initiate and stop the data collection.
+#' @return Path to a file with pprof samples.
+#' @param expr An R expression to profile.
+#' @param pprof Path to a file with pprof samples.
+#'   Also returned from the function.
+#' @examples
+#' # Returns a path to pprof samples.
+#' comingle_pprof(replicate(1e2, sample.int(1e4)))
+comingle_pprof <- function(expr, pprof = tempfile()) {
+  rprof <- comingle_rprof(expr)
+  proffer::to_pprof(rprof, pprof = pprof)
+  pprof
+}
+
+#' @title Profile R code and record Rprof samples.
+#' @export
+#' @description Run R code and record Rprof samples
+#'   from both R's and the native tracebacks.
+#'   This function uses [start_profiler()] and [stop_profiler()]
+#'   to initiate and stop the data collection.
+#' @return Path to a file with Rprof samples.
+#' @param expr An R expression to profile.
+#' @param rprof Path to a file with Rprof samples.
+#'   Also returned from the function.
+#' @examples
+#' # Returns a path to Rprof samples.
+#' comingle_rprof(replicate(1e2, sample.int(1e4)))
+comingle_rprof <- function(expr, rprof = tempfile()) {
+  on.exit(stop_profiler())
+  start_profiler(path = rprof)
+  expr
+  rprof
+}
