41a6b4e514100756368cbca83c4b1a9a51d267c3
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-14 04:43:59 +0200

    - Wrap `proffer::find_pprof()` instead of relying on `GOPATH`.

--- R/pprof.R
+++ R/pprof.R
@@ -3,58 +3,19 @@
 #' @description
 #' `pprof` is a tool for visualization and analysis of profiling data,
 #' implemented in Go.
-#' The `find_pprof()` function looks for the `pprof` executable
-#' in the `bin` subdirectory of the `GOPATH`
-#' (by default `~/go`, can be overridden by setting the `GOPATH`
-#' environment variable).
-#' It fails if no `pprof` executable can be found.
+#' This function is a wrapper around [proffer::find_pprof()]
+#' for determining its location on your system.
 #'
 #' This function will be called by [start_profiler()] to ensure
 #' that profiling data can be processed by [stop_profiler()].
 #' A functioning installation of `pprof` is a prerequisite to use
 #' this package.
 #'
-#' @section Installation:
-#'
-#' 1. Install Go 1.7 or newer (if necessary).
-#'
-#' 1. Install `pprof` into your `GOPATH`:
-#'
-#'     ```
-#'     go get github.com/google/pprof
-#'     ```
-#'
-#' See also the instructions on `pprof`'s
-#' [GitHub page](https://github.com/google/pprof#building-pprof).
-#'
-#'
 #' @export
 find_pprof <- function() {
-  gopath <- get_gopath()
-  path <- file.path(gopath, "bin", "pprof")
+  path <- proffer::pprof_path()
   if (!file.exists(path)) {
-    stop("Can't find pprof on your `GOPATH`. See `?find_pprof` for installation instructions.", call. = FALSE)
+    stop("Can't find `pprof`. See `?proffer::pprof_path` for installation instructions.", call. = FALSE)
   }
-  if (file.access(path, 1) != 0) {
-    stop("File ", path, " must have executable bit set. See `?find_pprof` for installation instructions.", call. = FALSE)
-  }
-
   path
-}
-
-.gopath_env <- new.env(parent = emptyenv())
-
-get_gopath <- function() {
-  if (is.null(.gopath_env$gopath)) {
-    if (nchar(Sys.which("go")) == 0) {
-      stop("The 'go' compiler tools must be installed. See `?find_pprof` for installation instructions.", call. = FALSE)
-    }
-    gopath <- system2("go", c("env", "GOPATH"), stdout = TRUE)
-    if (!dir.exists(gopath)) {
-      stop("`GOPATH` does not yet exist. See `?find_pprof` for installation instructions.", call. = FALSE)
-    }
-    .gopath_env$gopath <- gopath
-  }
-
-  .gopath_env$gopath
 }
